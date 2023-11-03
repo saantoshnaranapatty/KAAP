1c39c2e1410ddd28e3f720c0a1cb646d180b5801
Santosh Naranapatty <naranapatty.s@northeastern.edu>
2023-11-03 14:06:49 -0500

    Section 2: Course

--- main.py
+++ main.py
@@ -1,24 +1,58 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-#dont forget to secure this key
-api_key = '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'
+from langchain.chains import LLMChain, SequentialChain
+import argparse
+from dotenv import load_dotenv
 
-llm = OpenAI(
-    openai_api_key = api_key
-)
+load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--language", default = 'C++')
+parser.add_argument("--task", default = 'return a list of numbers')
+args = parser.parse_args()
+
+
+llm = OpenAI()
 
 code_prompt = PromptTemplate(
-    template="Write a very short {language} function that will {task}"
-    input_variables=['language', 'task']
+    template="Write a very short {language} function that will {task}",
+    input_variables=['language', 'task'],
+)
+
+#second prompt template 
+code_prompt_2 = PromptTemplate(
+    template="Write a test for the {language} code :\n{code}",
+    input_variables=['language', 'code']
 )
 
 code_chain = LLMChain(
     llm = llm,
-    prompt = code_prompt
+    prompt = code_prompt,
+    output_key= "code"
 )
 
+#second language model into which the prompt is going to be fed
+code_chain_2 = LLMChain(
+    llm = llm,
+    prompt = code_prompt_2,
+    output_key= "test"
+)
 
-result = llm('Write me a very short poem')
+#WE ARE TAKING THE OUTPUT OF CHAIN A AS THE INPUT TO CHAIN B
 
-print(result)
+#SEQUENTIAL CHAIN
+chain = SequentialChain(
+    chains = [code_chain,code_chain_2],
+    input_variables=['language', 'task'],
+    output_variables=['test','code']
+
+)
+
+result = chain({
+    "language":args.language,
+    "task":args.task
+}
+)
+
+print(result['code'])
+print(result['test'])
